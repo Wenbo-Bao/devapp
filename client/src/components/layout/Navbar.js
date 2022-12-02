b604eb40be9f60b72b30988d49926760b3504f22
Wenbo-Bao <wb2128@nyu.edu>
2022-12-02 12:51:21 -0500

    login authetication done

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,27 +1,52 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { logout } from '../../action/auth';
+import PropTypes from 'prop-types';
 
-export const Navbar = () => {
-  return (
-    <nav className='navbar bg-dark'>
-      <h1>
-        <Link to='/'>
-          <i className='fas fa-code'></i> DevConnector
-        </Link>
-      </h1>
-      <ul>
-        <li>
-          <a href='!#'>Developers</a>
-        </li>
-        <li>
-          <Link to='/register'>Register</Link>
-        </li>
-        <li>
-          <Link to='/login'>Login</Link>
-        </li>
-      </ul>
-    </nav>
-  );
+export const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
+    const authLinks = (
+        <ul>
+            <li>
+                <a onClick={logout} href='#!'>
+                    Logout
+                </a>
+            </li>
+        </ul>
+    );
+    const guestLinks = (
+        <ul>
+            <li>
+                <a href='#!'>Dev</a>
+            </li>
+            <li>
+                <Link to='/register'>Register</Link>
+            </li>
+            <li>
+                <Link to='/login'>Login</Link>
+            </li>
+        </ul>
+    );
+
+    return (
+        <nav className='navbar bg-dark'>
+            <h1>
+                <Link to='/'>Title</Link>
+            </h1>
+            {!loading && (
+                <Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>
+                // <Fragment>{authLinks}</Fragment>
+            )}
+        </nav>
+    );
 };
 
-export default Navbar;
+Navbar.proTypes = {
+    logout: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+    auth: state.auth,
+});
+export default connect(mapStateToProps, { logout })(Navbar);
